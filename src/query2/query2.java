322c2b671af806d92faed1c08e27df9dd3e454ba
Armand Halbert <armand.halbert@gmail.com>
2014-03-26 11:36:54 -0400

    Sort file added.

--- src/query2/query2.java
+++ src/query2/query2.java
@@ -1,7 +1,9 @@
 package query2;
 	    
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -25,7 +27,7 @@ public class query2 {
 
 	public void queries() {
 	    try {
-            BufferedReader file = new BufferedReader(new FileReader("./data/1k-sample-queries2.txt"));
+            BufferedReader file = new BufferedReader(new FileReader("./data/data.txt"));
             String s;
             Calendar c = Calendar.getInstance();
             while (( s = file.readLine() ) != null)  {
@@ -43,6 +45,28 @@ public class query2 {
 
             file.close();
         } catch (IOException e) { e.printStackTrace(); }
+	}
+
+	public void sortFile() {
+        TreeMap<String, String> bdates = new TreeMap<String, String>();
+        try {
+            BufferedReader file = new BufferedReader(new FileReader("data/person.csv"));
+            String s;
+            file.readLine();
+	        while (( s = file.readLine() ) != null)  {
+                String[] values = s.split("\\|");
+                String birthday = values[4];
+                bdates.put(birthday,s);
+	        }
+            file.close();
+            BufferedWriter wfile = new BufferedWriter(new FileWriter("data/person.csv"));
+            Iterator it = bdates.descendingMap().entrySet().iterator();
+            while(it.hasNext()) {
+                Map.Entry<String, String> kvpair = (Map.Entry)it.next();
+                wfile.write(kvpair.getValue()+"\n");
+            }
+            wfile.close();
+        } catch(Exception e) {}
 	}
 	
 	public ArrayList<String> eval(int k, Calendar d) {
@@ -63,14 +87,24 @@ public class query2 {
         }
         tags.putAll(tagmap);
         System.out.println(tags);
-
         it = tags.entrySet().iterator();
         
         ArrayList<String> ret = new ArrayList<String>();
+        int sz = -1;
         for (int i = 0; i < k; i++)  {
             Map.Entry<String,Integer> tag = (Map.Entry<String,Integer>)it.next();
+            sz = tag.getValue();
+            System.out.println(tag.getKey() + " "+ lookupTagDef(tag.getKey()));
             String s = lookupTagDef(tag.getKey());
             ret.add(s);
+        }
+
+        Map.Entry<String,Integer> tag = (Map.Entry<String,Integer>)it.next();
+        while(tag.getValue() == sz) {
+            System.out.println(tag.getKey() + " "+ lookupTagDef(tag.getKey()));
+            String s = lookupTagDef(tag.getKey());
+            ret.add(s);
+            tag = (Map.Entry<String,Integer>)it.next();
         }
         System.out.println(ret);
 	    return ret;
@@ -98,7 +132,6 @@ public class query2 {
      * Optimization: We essentially double-calculate the size of a tree. We should only do this once.
      * */ 
 	private ArrayList<Person> getTreesInForest(String interest) {
-	    int nodesLeft = people.size();
 	    HashMap<String, Person> peopleWithInterest = new HashMap<String, Person>();
 	    Iterator it = people.entrySet().iterator();
 	    while (it.hasNext()) {
@@ -111,20 +144,16 @@ public class query2 {
 	    it = peopleWithInterest.entrySet().iterator();
 	    ArrayList<Person> visited = new ArrayList<Person>();
 	    ArrayList<Person> trees = new ArrayList<Person>();
-	    Person s = null;
 	    while(it.hasNext()) {
             Map.Entry<String, Person> kvpair = (Map.Entry)it.next();
-            while (it.hasNext() && visited.contains(kvpair.getValue())) {
-                kvpair = (Map.Entry)it.next();
-            }
-            if (!it.hasNext()) 
-                break;
-            visited.add(kvpair.getValue());
-            trees.add(kvpair.getValue());
-            ArrayList<Person> v = getConnectedNodes(kvpair.getValue(), interest);
-            Iterator ita = v.iterator();
-            while(ita.hasNext()) {
-                visited.add((Person)ita.next());
+            if(!visited.contains(kvpair.getValue())) {
+                visited.add(kvpair.getValue());
+                trees.add(kvpair.getValue());
+                ArrayList<Person> v = getConnectedNodes(kvpair.getValue(), interest);
+                Iterator ita = v.iterator();
+                while(ita.hasNext()) {
+                    visited.add((Person)ita.next());
+                }
             }
         }
 	    return trees;
@@ -187,25 +216,25 @@ public class query2 {
      * */
     
 	private void loadPeople(Calendar d) {
+	    //Assumes file is sorted.
 	    try {
             BufferedReader file = new BufferedReader(new FileReader("data/person.csv"));
             String s;
             String[] values;
 
-            file.readLine();
-
 	        while (( s = file.readLine() ) != null)  {
                 values = s.split("\\|");
                 String birthday = values[4];
                 String id = values[0]; 
-
                 values = birthday.split("-");
                 Calendar birthdate = Calendar.getInstance();
                 birthdate.set(Integer.parseInt(values[0]), Integer.parseInt( values[1]), Integer.parseInt(values[2]));
 
-                if (birthdate.after(d)) {
-                    people.put(id, new Person(id));
+                if (birthdate.before(d)) {
+                    break;
                 }
+                people.put(id, new Person(id));
+
 	        }
             file.close();
 	    } catch(IOException e) { e.printStackTrace(); }
@@ -225,14 +254,17 @@ public class query2 {
                     p.addInterest(interest);
 
                 Integer  ientry;
-
-                if((ientry = map.get(interest)) == null)
-                    map.put(interest,0);
-                else
-                    map.put(interest, ientry + 1);
+                
+                    if((ientry = map.get(interest)) == null)
+                        map.put(interest,0);
+                    else
+                        if (p != null) {
+                            map.put(interest, ientry + 1);
+                    }
 	        }
             file.close();
             interestSize.putAll(map);
+            System.out.println(interestSize);
 	    } catch(IOException e) { e.printStackTrace(); }
 
 	    try {
